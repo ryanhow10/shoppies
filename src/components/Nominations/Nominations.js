375f8c21032c91ac634bf14c0571ee7a57482b14
Ryan How <ryanhow1999@yahoo.ca>
2020-09-03 16:22:38 -0400

    styling added

--- src/components/Nominations/Nominations.js
+++ src/components/Nominations/Nominations.js
@@ -5,10 +5,10 @@ class Nominations extends Component {
     render() {
         const { nominations, removeNomination } = this.props;
         const nominationsList = nominations.map(movie => {
-            return (<li className="nomination" key={ movie.imdbID }>{ movie.Title } ({ movie.Year }) <button type="button" onClick={ e => removeNomination(movie) }>Remove</button></li>)
+            return (<li className="nomination" key={ movie.imdbID }>{ movie.Title } ({ movie.Year }) <button type="button" className="removeButton" onClick={ e => removeNomination(movie) }>Remove</button></li>)
         });
         return ( 
-            <div className="nominationsComponent">
+            <div className="nominationsContainer">
                 <p className="nominationsTitle">Nominations</p>
                 <ul>{ nominationsList }</ul>
                 {
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -14,7 +14,7 @@ class Results extends Component {
     render() {
         const { finalSearchText, results, nominateMovie, nominations } = this.props;
         const resultList = results.map(movie => {
-            return (<li className="result" key={ movie.imdbID }>{ movie.Title } ({ movie.Year }) <button type="button" onClick={ e => nominateMovie(movie) } disabled={ this.isNominated(movie, nominations) || nominations.length === 5 }>Nominate</button></li>)
+            return (<li className="result" key={ movie.imdbID }>{ movie.Title } ({ movie.Year }) <button type="button" className="nominateButton" onClick={ e => nominateMovie(movie) } disabled={ this.isNominated(movie, nominations) || nominations.length === 5 }>Nominate</button></li>)
         });
         return ( 
             <div className="resultsContainer">
